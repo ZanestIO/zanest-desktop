b772f481280218e68727779e1671473645bb8267
Sadeq Sheikhi <sadeghshaikhi77@gmail.com>
2021-03-11 09:09:37 +0330

    LoginForm Validation and Sending Information to Main Process

---
+++ renderer/js/login.js
@@ -0,0 +1,71 @@
+const {ipcRenderer} = require('electron')
+var Vue = require('vue/dist/vue')
+
+const loginForm = document.querySelector('.login-box')
+const loginUsername = document.querySelector('#login-username')
+const loginPass = document.querySelector('#login-pass')
+const loginButton = document.querySelector('#login-button')
+
+let errors = {
+    error1: new Vue({
+        el: '#error1',
+        data: {
+            error: ''
+        }
+    }),
+    error2: new Vue({
+        el: '#error2',
+        data: {
+            error: ''
+        }
+    })
+}
+
+// adding eventHandler for clicking and hitting enter
+loginButton.addEventListener('click', async e => {
+    sendLoginInfo()
+})
+
+loginForm.addEventListener('keyup', async e=> {
+    if (e.code === 'Enter')
+        sendLoginInfo()
+})
+
+// Sending User Login Information Happens Here
+function sendLoginInfo() {
+    let username = loginUsername.value
+    let password = loginPass.value
+    let formValid = true
+
+    // removing error indicators when submit is fired
+    for (let [key, value] of Object.entries(errors)) {
+        value.error = ''
+    }
+
+    // username and password validation
+    if (!username) {
+        errors.error1.error = "نام کاربری نمی تواند خالی باشد"
+        formValid = false
+    }
+    if (!password) {
+        errors.error2.error = "رمز عبور نمی تواند خالی باشد"
+        formValid = false
+    } else if (password.length < 8) {
+        errors.error2.error = "رمزعبور شما کمتر از 8 کاراکتر نیست"
+        formValid = false
+    }
+
+    let send = ipcRenderer.send;
+    if (formValid)
+        send('userAuth', {username, password})
+
+}
+
+// getting response of user authentication from main
+ipcRenderer.on(`userAuthResponse`, (e, args) => {
+    console.log('from server' + args)
+})
+
+
+
+// removing content of
