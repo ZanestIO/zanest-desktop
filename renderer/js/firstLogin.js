a2b0842bf68efdcae86a037ae150c27d7ae53c5e
sadeqsheikhi <sadeghshaikhi77@gmail.com>
2021-03-22 16:26:10 +0430

    Adding FrontEnd Vue Integration to firstLogin

--- renderer/js/firstLogin.js
+++ renderer/js/firstLogin.js
@@ -1,67 +1,101 @@
 const {ipcRenderer} = require('electron')
-var Vue = require('vue/dist/vue.min')
+const Vue = require('vue')
 const notListener = require('./notListener');
 // setting up notification listeners
 notListener()
 
 loadingIcon = '<i class="fas fa-circle-notch fa-spin text-lg"></i>'
 
-let userName = new Vue({
-    el:'#sign-username',
-    data: {
-        err: false
-    }
-})
-
-let password = new Vue( {
-    el: '#sign-pass',
-    data: {
-        err: false,
-    }
-})
-
-let passwordRepeat = new Vue( {
-    el:'#sign-pass',
-    data: {
-        data: {
-            err: false,
+const signinBox = {
+    data() {
+        return {
+            username: {
+                err: false,
+                value: ''
+            },
+            usernameErr: {
+                seen: false,
+                text: ''
+            },
+            progress: {
+                seen: false,
+                width: 'w-1/4'
+            },
+            password: {
+                value: ''
+            },
+            passwordRepeat: {
+                value: '',
+                err: false
+            },
+            passwordRepeatErr: {
+                seen: false,
+                text: ''
+            }
         }
-    }
-})
+    },
+    methods: {
+        processUsername() {
+            let regex = new RegExp('^[A-Za-z0-9_-]*$')
+            if (! regex.test(this.username.value) ) {
+                this.username.err = true
+                this.usernameErr.seen = true
+                this.usernameErr.text = "کاراکتر غیر مجاز"
+            } else {
+                this.username.err = false
+                this.usernameErr.seen = false
+            }
+        }, // process username
+        processPassword() {
+            let count = 1
+            if(this.password.value.length >= 8) {
+                count = 2
+                let smallLetters = new RegExp('[a-z]')
+                let capitalLetters = new RegExp('[A-Z]')
+                let numbers = new RegExp('[0-9]')
+                let symbols = new RegExp('[-!#$%^&*()_+|~=`{}\\[\\]:";\'<>?,.\\/]')
 
-let button = new Vue({
-    el:'#register',
-    data: {
-        text: 'ایجاد حساب کاربری'
-    }
-})
+                if(capitalLetters.test(this.password.value) || symbols.test(this.password.value)) {
+                    count++
+                }
 
-let progressHolder = new Vue({
-    el: '#pass-bar-holder',
-    data: {
-        seen: false
-    }
-})
+                if(numbers.test(this.password.value) && smallLetters.test(this.password.value)) {
+                    count++
+                }
+            }
 
-let progress = new Vue({
-    el: '#pass-bar',
-    data: {
-        width: 'w0'
-    }
-})
+            this.progress.seen = true
+            switch (count) {
+                case 1:
+                    this.progress.width = 'w-1/4'
+                    break
+                case 2:
+                    this.progress.width = 'w-1/2'
+                    break
+                case 3:
+                    this.progress.width = 'w-3/4'
+                    break
+                case 4:
+                    this.progress.width = 'w-full'
 
-let usernameErr = new Vue({
-    el: '#username-err',
-    data: {
-        seen: false,
-        text: ''
+            }
+        }, // process password
+        processPassRep() {
+            if ( this.password.value !== this.passwordRepeat.value) {
+                this.passwordRepeat.err = true
+                this.passwordRepeatErr.seen = true
+                this.passwordRepeatErr.text = 'رمز عبور و تکرار آن یکسان نیستند'
+            } else {
+                this.passwordRepeat.err = false
+                this.passwordRepeatErr.seen = false
+            }
+        },
     }
-})
+}
 
-let passErr = new Vue({
-    el: '#pass-err',
-    data: {
-        seen: false,
-        text: ""
-    }
-})
+Vue.createApp(signinBox).mount('#signin-box')
+
+
+
+
+// validate inputs in
