09f39b1047fa834ad8339ab045ddd5eae4e3b4b2
sadeqsheikhi <sadeghshaikhi77@gmail.com>
2021-05-01 15:10:55 +0430

    Pagination FrontEnd Implemented

--- renderer/js/components/students/add_student.js
+++ renderer/js/components/students/add_student.js
@@ -1,18 +1,10 @@
 const {ipcRenderer} = require('electron')
-const confirm_alert = require('./confirmAlert')
-const Vue = require('vue')
-const {resetError, isEmpty, exact, smallerThan, biggerThan, isNumber, isLetter} = require('./../utils/validation')
+const {resetError, isEmpty, exact, smallerThan, biggerThan, isNumber, isLetter} = require('../../utils/validation')
 
 module.exports = {
     data() {
         return {
             valid: true,
-            changed: false,
-            deleteBox: {
-                seen: false,
-                title: 'آیا مطمئن هستید؟',
-                desc: 'با تایید عملیات زبان آموز فعلی از سیستم حذف خواهد شد'
-            },
             name: {
                 err: false,
                 value: '',
@@ -77,56 +69,23 @@ module.exports = {
             },
         }
     },
-    provide() {
-        return {
-            // providing for notifications
-            seen: Vue.computed(() => this.deleteBox.seen),
-            error: Vue.computed(() => this.deleteBox.title),
-            error_desc: Vue.computed(() => this.deleteBox.desc),
-        }
-    },
-    components: {
-        confirm_alert,
-    },
-    created() {
-        ipcRenderer.on('getInfo', (e, args) => {
-            this.name.value = args.fullName
-            this.phone.value = args.phoneNumber
-            this.sex.value = args.sex
-            this.sid.value = args.socialID
-            this.parentName.value = args.parentsName
-            this.parentPhone.value = args.parentNumber
-            this.address.value = args.address
-
-            // setting the oldSid
-            this.oldSid = args.socialID
-
-            // handling the date
-            let date = args.birthDate.split('/')
-            this.birthDate.year.value = date[0]
-            this.birthDate.month.value = date[1]
-            this.birthDate.day.value = date[2]
-        })
-    },
+    inject: ['addSeen'],
+    emits: ['cancelAdd'],
     methods: {
         processName() {
             let input = this.name
             resetError(input)
-            this.changed = true
 
             if (isEmpty(input)) {
                 this.valid = false
             } else if (isLetter(input)) {
                 this.valid = false
-            } else {
+            } else
                 input.success = true
-                this.valid = true
-            }
         },
         processPhone() {
             let input = this.phone
             resetError(input)
-            this.changed = true
 
             if (isEmpty(input)) {
                 this.valid = false
@@ -134,44 +93,35 @@ module.exports = {
                 this.valid = false
             } else if (exact(input, 11)) {
                 this.valid = false
-            } else {
+            } else
                 input.success = true
-                this.valid = true
-            }
         },
         processSex() {
             let input = this.sex
             resetError(input)
-            this.changed = true
 
             if (isEmpty(input)) {
                 this.valid = false
-            } else {
+            } else
                 input.success = true
-                this.valid = true
-            }
         },
 
         processSid() {
             let input = this.sid
             resetError(input)
-            this.changed = true
 
             if (isEmpty(input)) {
                 this.valid = false
             } else if (exact(input, 10)) {
                 this.valid = false
-            } else {
+            } else
                 input.success = true
-                this.valid = true
-            }
         },
 
 
         processBirthDay() {
             let input = this.birthDate.day
             resetError(input)
-            this.changed = true
             if (isEmpty(input)) {
                 this.valid = false
             } else if (smallerThan(input, 1)) {
@@ -180,14 +130,12 @@ module.exports = {
                 this.valid = false
             } else {
                 input.success = true
-                    this.valid = true
             }
         },
 
         processBirthMonth() {
             let input = this.birthDate.month
             resetError(input)
-            this.changed = true
             if (isEmpty(input)) {
                 this.valid = false
             } else if (smallerThan(input, 1)) {
@@ -196,13 +144,11 @@ module.exports = {
                 this.valid = false
             } else {
                 input.success = true
-                    this.valid = true
             }
         },
         processBirthYear() {
             let input = this.birthDate.year
             resetError(input)
-            this.changed = true
             if (isEmpty(input)) {
                 this.valid = false
             } else if (smallerThan(input, 1300)) {
@@ -211,26 +157,22 @@ module.exports = {
                 this.valid = false
             } else {
                 input.success = true
-                    this.valid = true
             }
         },
         processParentName() {
             let input = this.parentName
             resetError(input)
-            this.changed = true
             if (isEmpty(input)) {
                 this.valid = false
             } else if (isLetter(input)) {
                 this.valid = false
             } else {
                 input.success = true
-                    this.valid = true
             }
         },
         processParentPhone() {
             let input = this.parentPhone
             resetError(input)
-            this.changed = true
 
             if (isEmpty(input)) {
                 this.valid = false
@@ -238,22 +180,17 @@ module.exports = {
                 this.valid = false
             } else if (exact(input, 11)) {
                 this.valid = false
-            } else {
+            } else
                 input.success = true
-                this.valid = true
-            }
         },
         processAddress() {
             let input = this.address
             resetError(input)
-            this.changed = true
 
             if (isEmpty(input)) {
                 this.valid = false
-            } else {
+            } else
                 input.success = true
-                this.valid = true
-            }
         },
 
         processAll() {
@@ -273,11 +210,10 @@ module.exports = {
         // ==================================================================================
         // final submit and sending the values
         // ==================================================================================
-        edit() {
-            if (this.valid && this.changed) {
-
-                ipcRenderer.send('studentUpdate', {
-                    oldSid: this.oldSid,
+        submit() {
+            this.processAll()
+            if (this.valid) {
+                ipcRenderer.send('studentCreation', {
                     fullName: this.name.value,
                     socialID: this.sid.value,
                     parentsName: this.parentName.value,
@@ -288,22 +224,28 @@ module.exports = {
                     address: this.address.value
                 })
 
-                this.changed = false
-            }
-        },
+                // clearing the fields for another creation
+                // this.name.value = ''
+                // this.phone.value = ''
+                // this.sid.value = ''
+                // this.parentName.value = ''
+                // this.parentPhone.value = ''
+                // this.address.value = ''
+                // this.birthDate.day.value = ''
+                // this.birthDate.month.value = ''
+                // this.birthDate.year.value = ''
 
-        // ==================================================================================
-        // requesting delete user
-        // ==================================================================================
-        deleteUser() {
-            this.deleteBox.seen = true
-        },
-        confirm_delete() {
-            this.deleteBox.seen = false
-            ipcRenderer.send('studentDeletion',  this.sid.value)
-        },
-        cancelDelete() {
-            this.deleteBox.seen = false
+                // resetting every field
+                resetError(this.name)
+                resetError(this.phone)
+                resetError(this.sid)
+                resetError(this.parentName)
+                resetError(this.parentPhone)
+                resetError(this.address)
+                resetError(this.birthDate.day)
+                resetError(this.birthDate.month)
+                resetError(this.birthDate.year)
+            }
         }
 
 
@@ -313,15 +255,13 @@ module.exports = {
     // Template
     // ==================================================================================
     template: `
-      <section class="big-section">
-      <confirm_alert @confirm="confirm_delete" @cancel-box="cancelDelete"></confirm_alert>
+      <section class="big-section" v-if="addSeen.value">
       <div class="full-edit-box">
         <div>
           <span>
             نام
           </span>
-          <input type="text" placeholder="نام و نام خانوادگی" :class="{fail: name.err, success: name.success}"
-                 v-model="name.value"
+          <input type="text" placeholder="نام و نام خانوادگی" :class="{fail: name.err, success: name.success}" v-model="name.value"
                  v-on:change="processName">
           <p class="input-error" v-if="name.err">{{ name.errMsg }}</p>
         </div>
@@ -330,8 +270,7 @@ module.exports = {
           <span>
             شماره تماس
           </span>
-          <input type="number" placeholder="(زبان آموز)" :class="{fail: phone.err, success: phone.success}"
-                 v-model="phone.value"
+          <input type="number" placeholder="(زبان آموز)" :class="{fail: phone.err, success: phone.success}" v-model="phone.value"
                  v-on:change="processPhone">
           <p class="input-error" v-if="phone.err">{{ phone.errMsg }}</p>
         </div>
@@ -340,7 +279,7 @@ module.exports = {
           <span>
             جنسیت
           </span>
-          <select :class="{fail: sex.err, success: sex.success}" v-model="sex.value" v-on:change="processSex">
+          <select :class="{fail: sex.err, success: sex.success}" v-model="sex.value" @change="processSex">
             <option value="male">مرد</option>
             <option value="female">زن</option>
             <option value="bisex">نامشخص</option>
@@ -352,8 +291,7 @@ module.exports = {
           <span>
             کدملی
           </span>
-          <input type="number" placeholder="--" :class="{fail: sid.err, success: sid.success}" v-model="sid.value"
-                 v-on:change="processSid">
+          <input type="number" placeholder="--" :class="{fail: sid.err, success: sid.success}" v-model="sid.value" @change="processSid">
           <p class="input-error" v-if="sid.err">{{ sid.errMsg }}</p>
         </div>
 
@@ -363,23 +301,20 @@ module.exports = {
           </span>
           <div class="input-group">
             <div>
-              <input type="number" placeholder="روز" min="1" max="31"
-                     :class="{fail: birthDate.day.err, success: birthDate.day.success}"
-                     v-model="birthDate.day.value" v-on:change="processBirthDay">
+              <input type="number" placeholder="روز" min="1" max="31" :class="{fail: birthDate.day.err, success: birthDate.day.success}"
+                     v-model="birthDate.day.value" @change="processBirthDay">
               <p class="input-error" v-if="birthDate.day.err">{{ birthDate.day.errMsg }}</p>
             </div>
 
             <div>
-              <input type="number" placeholder="ماه" min="1" max="12"
-                     :class="{fail: birthDate.month.err, success: birthDate.month.success}"
-                     v-model="birthDate.month.value" v-on:change="processBirthMonth">
+              <input type="number" placeholder="ماه" min="1" max="12" :class="{fail: birthDate.month.err, success: birthDate.month.success}"
+                     v-model="birthDate.month.value" @change="processBirthMonth">
               <p class="input-error" v-if="birthDate.month.err">{{ birthDate.month.errMsg }}</p>
             </div>
 
             <div>
-              <input type="number" placeholder="سال"
-                     :class="{fail: birthDate.year.err,  success: birthDate.year.success}"
-                     v-model="birthDate.year.value" v-on:change="processBirthYear">
+              <input type="number" placeholder="سال" min='1300' max="1450" :class="{fail: birthDate.year.err,  success: birthDate.year.success}"
+                     v-model="birthDate.year.value" @change="processBirthYear">
               <p class="input-error" v-if="birthDate.year.err">{{ birthDate.year.errMsg }}</p>
             </div>
           </div>
@@ -389,8 +324,7 @@ module.exports = {
           <span>
             نام والد
           </span>
-          <input type="text" placeholder="(پدر یا مادر)" :class="{fail: parentName.err, success: parentName.success}"
-                 v-model="parentName.value"
+          <input type="text" placeholder="(پدر یا مادر)" :class="{fail: parentName.err, success: parentName.success}" v-model="parentName.value"
                  @change="processParentName">
           <p class="input-error" v-if="parentName.err">{{ parentName.errMsg }}</p>
         </div>
@@ -399,8 +333,7 @@ module.exports = {
           <span>
             شماره تماس والد
           </span>
-          <input type="number" placeholder="(پدر یا مادر)"
-                 :class="{fail: parentPhone.err , success: parentPhone.success}" v-model="parentPhone.value"
+          <input type="number" placeholder="(پدر یا مادر)" :class="{fail: parentPhone.err , success: parentPhone.success}" v-model="parentPhone.value"
                  @change="processParentPhone">
           <p class="input-error" v-if="parentPhone.err">{{ parentPhone.errMsg }}</p>
         </div>
@@ -409,19 +342,18 @@ module.exports = {
           <span>
             آدرس
           </span>
-          <input type="text" placeholder="محل سکونت" :class="{fail: address.err, success: address.success}"
-                 v-model="address.value"
+          <input type="text" placeholder="محل سکونت" :class="{fail: address.err, success: address.success}" v-model="address.value"
                  @change="processAddress">
           <p class="input-error" v-if="address.err">{{ address.errMsg }}</p>
         </div>
       </div>
 
-      <div class="px-4 w-full flex justify-between">
-        <button class="btn-mid ml-10" :class="{'btn-success': changed, 'btn-muted': !changed}" v-on:click="edit">
-          ویرایش
+      <div class="px-4 flex items-end">
+        <button class="btn btn-mid btn-primary ml-8" v-on:click="submit">
+          ایجاد زبان آموز
         </button>
-        <button class="btn-delete btn-mid" v-on:click="deleteUser">
-          حذف
+        <button class="btn btn-mid btn-cancel" v-on:click="$emit('cancelAdd')">
+          انصراف
         </button>
       </div>
       </section>
