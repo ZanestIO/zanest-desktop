0c5cd4781b2a0d5bbc8ee2a06cb80166eeb62634
negin <anegin4@gmail.com>
2021-05-02 11:22:13 +0430

    testing addStd() added

--- test/models/test.js
+++ test/models/test.js
@@ -1,7 +1,7 @@
 const {expect} = require('chai');
 const sinon = require("sinon");
 const db = require('../../models/Db');
-const Student = require('../../models/Student/Student');
+const {Student} = require('../../models/Student/Student');
 
 describe("#Models", function () {
 
@@ -13,14 +13,10 @@ describe("#Models", function () {
         }).then(() => done())
     });
 
-    after(function (done) {
-        testdb.sequelize.drop().then(function () {
-        }).then(done, done);
-    });
 
     describe("#StudentModel", function () {
 
-        // initialize student
+        // initialize student data
         const studentData = {
             socialID: 3721584975,
             parentName: "ali",
@@ -33,18 +29,24 @@ describe("#Models", function () {
         }
 
         describe("#addStd", function () {
-            // db().init()
 
             it('should add new student to database', async function () {
-                // console.log(db().models)
-                // console.log(testdb)
-                // const add = require('../../models/Student/add')
+
                 const stub = sinon.stub(Student, "add").returns(studentData);
                 const result = await db().sequelize.models.Student.add(studentData);
 
                 expect(stub.calledOnce).to.be.true;
 
-
+                expect(result.socialID).to.equal(studentData.socialID);
+                expect(result.parentName).to.equal(studentData.parentName);
+                expect(result.parentNumber).to.equal(studentData.parentNumber);
+                expect(result.fullName).to.equal(studentData.fullName);
+                expect(result.sex).to.equal(studentData.sex);
+                expect(result.phoneNumber).to.equal(studentData.phoneNumber);
+                expect(result.birthDate).to.equal(studentData.birthDate);
+                expect(result.address).to.equal(studentData.address);
+                expect(result.createdAt).to.equal(studentData.createdAt);
+                expect(result.updatedAt).to.equal(studentData.updatedAt);
             });
         });
     });
